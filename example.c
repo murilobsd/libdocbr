81ecfab6ae9319cf9a5dc52e3510928365aa830a
murilo ijanc <mbsd@m0x.ru>
2020-01-26 10:44:21 -0300

    update example

--- example.c
+++ example.c
@@ -14,38 +14,34 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "docbr.h"
 
+
+static int invalid_cpf[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1};
+static int valid_cpf[11] = {5, 9, 5, 9, 9, 2, 4, 9, 1, 9, 4};
+static int invalid_cnpj[14] = {5, 2, 4, 7, 2, 7, 7, 1, 0, 0, 0, 1, 8, 1};
+static int valid_cnpj[14] = {5, 2, 4, 7, 2, 7, 7, 1, 0, 0, 0, 1, 8, 5};
+static int invalid_ddd = 100;
+static int valid_ddd = 16;
+
 int
 main(int argc, char *argv[])
 {
-	int cpf_number[11] = {0};
-	int cpf_len = 0, cpf_status = 0;
-	char p;
+	int status = 0;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <cpf_number>\n", argv[0]);
-		exit(1);
-	}
-
-	cpf_len = strlen(argv[1]);
-	if (cpf_len != 11) {
-		fprintf(stderr, "invalid cpf number, eg: 12345678911\n");
-		exit(1);
-	}
-
-	for (size_t i = 0; i < strlen(argv[1]); i++) {
-		p = argv[1][i];
-		cpf_number[i] = atoi(&p);
-	}
-
-	if ((cpf_status = cpf_isvalid(cpf_number)) != -1) {
+	if ((status = cpf_isvalid(invalid_cpf)) == -1)
+		printf("CPF is invalid\n");
+	if  ((status = cpf_isvalid(valid_cpf)) != -1)
 		printf("CPF is valid\n");
-		return (EXIT_SUCCESS);
-	}
-	printf("CPF: %s is invalid\n", argv[1]);
-	return (EXIT_FAILURE);
+	if  ((status = cnpj_isvalid(invalid_cnpj)) == -1)
+		printf("CNPJ is invalid\n");
+	if  ((status = cnpj_isvalid(valid_cnpj)) != -1)
+		printf("CNPJ is valid\n");
+	if  ((status = ddd_isvalid(invalid_ddd)) == -1)
+		printf("DDD is invalid\n");
+	if  ((status = ddd_isvalid(valid_ddd)) != -1)
+		printf("DDD is valid\n");
+	return (0);
 }
